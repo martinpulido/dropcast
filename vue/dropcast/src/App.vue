0a1bec6546e0c4e713dff13ccf212c28717f95a9
martinpulido <smartinpulido@gmail.com>
2018-11-22 12:21:58 +0100

    Add base HTML / assets

--- vue/dropcast/src/App.vue
+++ vue/dropcast/src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="site__bg"></div>
+    <div class="main">
+
+      <nav>
+        <div class="logo nav__logo">
+          <a href="index.html"><img src="assets/images/logo.svg" alt="logo"/></a>
+        </div>
+        <ul class="nav__list">
+          <li class="nav__item"><a href="#">Episodes</a></li>
+        </ul>
+        <ul class="nav__social">
+          <li><a href="#" class="nav_social__item"><i class="fab fa-google-play"></i></a></li>
+          <li><a href="#" class="nav_social__item"><i class="fab fa-itunes-note"></i></a></li>
+          <li><a href="#" class="nav_social__item"><i class="fas fa-rss-square"></i></a></li>
+        </ul>
+      </nav>
+      
+    </div>
+    
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <footer>
+      <p class="footer__content">Dropcast is a template made by <a href="https://amie-chen.com">Amie Chen</a> exclusively for Codrops</p>
+      <div class="logo footer__logo">
+        <a href="https://tympanus.net/codrops/"><img src="./assets/images/logo-white.svg" alt="logo"/></a>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -31,30 +42,8 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  @import "src/assets/scss/variable.scss";
+  @import "src/assets/scss/animation.scss";
+  @import "src/assets/scss/base.scss";
+  @import "src/assets/scss/site.scss";
 </style>
