6eb739fc916de8de53344bc253f2dd2e8e524abd
martinpulido <smartinpulido@gmail.com>
2018-12-12 11:23:56 +0100

    Update webpack.config.js to load mp3 / ogg files.

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|gif|svg|mp3|ogg)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
