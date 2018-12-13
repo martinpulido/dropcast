f6ef9bfc5843dcb29959db268152bb6dd499d200
martinpulido <smartinpulido@gmail.com>
2018-12-13 18:10:16 +0100

    Fix conditional Prev / Next episode.

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,16 +1,21 @@
 <template>
 
   <div class="site__navigation">
+    <div v-if="prevEpisode !== null">
+      <router-link :to="{name: 'episode-detail', params:{id: prevEpisode.id}}" class="site__navigation_items site__navigation_items--left" :class="[(prevEpisode == null)? 'hidden' : '']">
+        <span class="subtitle">{{this.previousText}}</span>
+        <h4 class="title" v-html="prevEpisode.full_name"></h4>
+      </router-link>
+    </div>
+    <div v-else></div>
     
-    <router-link :to="{name: 'episode-detail', params:{id: prevEpisode.id}}" class="site__navigation_items site__navigation_items--left" :class="[(prevEpisode == null)? 'hidden' : '']">
-      <span class="subtitle">{{this.previousText}}</span>
-      <h4 class="title" v-html="prevEpisode.full_name"></h4>
-    </router-link>
-    
-    <router-link :to="{name: 'episode-detail', params:{id: nextEpisode.id}}" class="site__navigation_items site__navigation_items--right" :class="[(nextEpisode == null)? 'hidden' : '']">
-      <span class="subtitle">{{this.nextText}}</span>
-      <h4 class="title" v-html="nextEpisode.full_name"></h4>
-    </router-link>
+    <div v-if="nextEpisode !== null">
+      <router-link :to="{name: 'episode-detail', params:{id: nextEpisode.id}}" class="site__navigation_items site__navigation_items--right" :class="[(nextEpisode == null)? 'hidden' : '']">
+        <span class="subtitle">{{this.nextText}}</span>
+        <h4 class="title" v-html="nextEpisode.full_name"></h4>
+      </router-link>
+    </div>
+    <div v-else></div>
   </div>
 
 </template>
